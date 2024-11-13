19fa08c29bdcaeb23fc8976d44b333860eb12672
Baidya Sarkar <bns@outlook.in>
2024-11-12 20:10:27 -0500

    Mission go online

---
+++ src/main/resources/static/scripts/paintings.js
@@ -0,0 +1,63 @@
+// Step 1: Array of image file names (assuming they are all in the 'img' folder)
+const imageFiles = [
+	"art-1.jpg",
+	"art-2.jpg",
+	"art-3.jpg",
+	"art-4.jpg",
+	"art-5.jpg",
+	"art-6.jpg",
+	"art-7.jpg",
+	"art-8.jpg"
+];
+
+// Function to display images
+function loadPaintings() {
+
+	const container = document.getElementById("image-container");
+	imageFiles.forEach(fileName => {
+		let imageLocation = `/arts/${fileName}`;
+		
+		const imageBox = document.createElement("div");
+		imageBox.classList.add("image-box");
+
+		const img = document.createElement("img");
+		img.classList.add("clickable-image");
+		img.src = imageLocation; // Adjust path as needed
+		img.alt = fileName;
+		img.onclick = () => {
+			enlargeImage(imageLocation);
+		};
+		// Create div for watermark
+		const watermark = document.createElement("div");
+		watermark.classList.add("watermark");
+		// Append the image and watermark the image box, and the image box to the container
+		imageBox.appendChild(img);
+		imageBox.appendChild(watermark);
+
+		container.appendChild(imageBox);
+	});
+
+	const lang = document.documentElement.lang;
+	const watermarkDivs = document.querySelectorAll('.watermark');
+	watermarkDivs.forEach(div => {
+		if (lang === "bn") {
+			div.textContent = "প্রতিভার ক্যানভাস";
+		} else {
+			div.textContent = "Prativa on Canvas"; // Set the content for each div
+		}
+	});
+}
+
+// Define the function to be called on click
+function enlargeImage(imgSource) {
+	var modal = document.getElementById("modal");
+	var modalImg = document.getElementById("modal-img");
+
+	modal.style.display = "block";
+	modalImg.src = imgSource;
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeImage() {
+	modal.style.display = "none";
+}
